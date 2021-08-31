01d7fed154edc71be256d2041d2b77b71e8cc6fb
Shane Thurston <36428573+shane-thurston@users.noreply.github.com>
2021-08-31 14:34:37 +1000

    Update encouragements.py

--- encouragements.py
+++ encouragements.py
@@ -17,3 +17,19 @@ if msg.startswith('$new'):
       await message.channel.send("New encouraging message added.")
     else:
       await message.channel.send("Sorry couldn't add that.")
+
+#code to create helper function remove encouragements to our existing list
+def remove_encouragements(enc_msg):
+  if enc_msg in starter_encouragements:
+    starter_encouragements.remove(enc_msg)
+    return True
+  else:
+    return False
+
+#code to send remove encouragements to the helper function
+if msg.startswith('$delete'):
+    remove_message = msg.split('$delete ',1)[1]
+    if remove_encouragements(remove_message):
+      await message.channel.send("Message deleted.")
+    else:
+      await message.channel.send("Sorry couldn't delete that.")
