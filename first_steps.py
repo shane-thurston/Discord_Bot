2bec07eed4457760a0bb1149f8e4cd425dbdfda4
Shane Thurston <36428573+shane-thurston@users.noreply.github.com>
2022-08-15 08:25:57 +1000

    Create first_steps.py

---
+++ first_steps.py
@@ -0,0 +1,27 @@
+#underneath the new Discord instance
+sad_words = [
+  "sad", 
+  "depressed", 
+  "unhappy", 
+  "angry", 
+  "miserable", 
+  "depressing"
+]
+
+starter_encouragements = [
+  "Cheer up!",
+  "Hang in there.",
+  "You are a great person / bot!"
+]
+
+#inside main msg loop
+if any(word in msg for word in sad_words):
+    await message.channel.send(random.choice(starter_encouragements))
+    
+if msg.startswith('$hello'):
+    await message.channel.send('Hello')
+    emoji = '\N{THUMBS UP SIGN}'
+    await message.add_reaction(emoji)
+    
+if any(word in msg for word in harm_words):
+    await message.author.send("Suicide website link")
